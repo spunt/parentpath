b30c61cd807055beef62302e3dcacf8df4e82ecb
Bob Spunt <bobspunt@gmail.com>
2015-05-11 10:12:46 -0700

    initial commit

---
+++ parentpath.m
@@ -0,0 +1,25 @@
+function parpath = parentpath(subpaths)
+% PARENTPATH Find parent path from multiple subpaths
+%
+%   USAGE:      parpath = parentpath(subpaths)
+% __________________________________________________________________________
+%   SUBPATHS:   CHAR or CELL array containing strings for multiple paths
+%
+
+% ---------------------- Copyright (C) 2015 Bob Spunt ----------------------
+%	Created:  2015-03-27
+%	Email:    spunt@caltech.edu
+% __________________________________________________________________________
+if      nargin < 1, disp('USAGE: parpath = parentpath(subpaths)'); return; end
+if      iscell(subpaths), subpaths = char(subpaths); end
+if      size(subpaths, 1)==1, disp('Only one subpath! Doing nothing...'); return; end
+
+% | Get indices of noncommon characters
+if      size(subpaths, 1)==2, diffidx = find(diff(subpaths));
+else    diffidx = find(sum(diff(subpaths))); end
+
+% | Assign parent path
+parpath = fileparts(subpaths(1, 1:diffidx(1)));
+
+end
+
